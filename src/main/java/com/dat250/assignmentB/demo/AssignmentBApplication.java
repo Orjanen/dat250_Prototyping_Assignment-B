a97c7fa64fd33821ddbc017d011f79f860c03171
orjanen <superszura@gmail.com>
2020-09-23 09:41:08 +0200

    init

---
+++ src/main/java/com/dat250/assignmentB/demo/AssignmentBApplication.java
@@ -0,0 +1,13 @@
+package com.dat250.assignmentB.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dat250/assignmentB/demo/AssignmentBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat250.assignmentB.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
