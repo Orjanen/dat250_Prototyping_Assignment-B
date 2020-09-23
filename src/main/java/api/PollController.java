edf67ef28aa5ec426df8ff75ffc4b677bb0008e4
Tohovi <31278784+h119653@users.noreply.github.com>
2020-09-23 09:53:54 +0200

    PollController, VoteController

---
+++ src/main/java/api/PollController.java
@@ -0,0 +1,11 @@
+package api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/poll")
+@RestController
+public class PollController {
+
+}
