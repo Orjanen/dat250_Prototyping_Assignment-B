484a31d8da60197da5ca2b63cc19aee07fefdcf3
RogerWisnes <rogerwisnes@gmail.com>
2020-10-20 21:47:34 +0200

    Added gson-dependency to pom, and started addig json-converting to Receiver

--- api/src/main/java/com/restdeveloper/app/ws/publisher/Receiver.java
+++ api/src/main/java/com/restdeveloper/app/ws/publisher/Receiver.java
@@ -1,7 +1,10 @@
 package com.restdeveloper.app.ws.publisher;
 
 import java.util.concurrent.CountDownLatch;
+
 import org.springframework.stereotype.Component;
+import com.google.gson.Gson;
+
 
 @Component
 public class Receiver {
@@ -10,8 +13,31 @@ public class Receiver {
 
     public void receiveMessage(Object message) {
         System.out.print("Received message:\n\t");
-        System.out.println(message);
+        processMessage(message);
         latch.countDown();
+    }
+
+    private void processMessage(Object object) {
+        String type = object.getClass().getSimpleName();
+        System.out.println(type);
+        //String jsonFormat = new Gson().toJson(object);
+        System.out.println("jsonFormat");
+        System.out.println("\n");
+        switch (type) {
+            case "VoteEntity":
+                System.out.println("VoteEntity:\n" + "jsonFormat");
+                break;
+            case "PollEntity":
+                System.out.println("PollEntity:\n" + "jsonFormat");
+                break;
+            case "UserEntity":
+                System.out.println("UserEntity:\n" + "jsonFormat");
+                break;
+            default:
+                throw new IllegalArgumentException("Message is not an entity");
+
+        }
+
     }
 
     public CountDownLatch getLatch() {
