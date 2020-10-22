65df4849f59dbfb0f1e3b96573aa275f7d9bfab3
RogerWisnes <rogerwisnes@gmail.com>
2020-10-22 23:44:17 +0200

    Removed unused code

--- api/src/main/java/com/restdeveloper/app/ws/publisher/rabbitmq/Receiver.java
+++ api/src/main/java/com/restdeveloper/app/ws/publisher/rabbitmq/Receiver.java
@@ -5,18 +5,9 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 public class Receiver {
 
     @RabbitListener(queues = "#{autoDeleteQueue.name}")
-    public void receive(String message) throws InterruptedException {
+    public void receive(String message) {
         System.out.println("Api-Receiver got message:");
         System.out.println(message);
-        doWork(message);
-    }
-
-    private void doWork(String message) throws InterruptedException {
-        for (char ch : message.toCharArray()) {
-            if (ch == '.') {
-                Thread.sleep(1000);
-            }
-        }
     }
 
 }
