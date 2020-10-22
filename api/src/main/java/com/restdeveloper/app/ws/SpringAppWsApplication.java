1eaaec4f37a8fab2f71008ba5c9a5a01890e794a
RogerWisnes <rogerwisnes@gmail.com>
2020-10-22 22:11:37 +0200

    Cleaned up, and removed unused code

--- api/src/main/java/com/restdeveloper/app/ws/SpringAppWsApplication.java
+++ api/src/main/java/com/restdeveloper/app/ws/SpringAppWsApplication.java
@@ -1,13 +1,5 @@
 package com.restdeveloper.app.ws;
 
-import com.restdeveloper.app.ws.publisher.Receiver;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,51 +8,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class SpringAppWsApplication {
 
-	public static final String topicExchangeName = "spring-boot-exchange";
-	public static final String queueName = "spring-boot";
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppWsApplication.class, args);
+    }
 
-	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
-	}
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
-	}
-
-	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-	}
-
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-											 MessageListenerAdapter listenerAdapter) {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
-		return container;
-	}
-
-	@Bean
-	MessageListenerAdapter listenerAdapter(Receiver receiver) {
-		return new MessageListenerAdapter(receiver, "receiveMessage");
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAppWsApplication.class, args);
-	}
-
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public SpringApplicationContext springApplicationContext(){
-		return new SpringApplicationContext();
-	}
+    @Bean
+    public SpringApplicationContext springApplicationContext() {
+        return new SpringApplicationContext();
+    }
 
 }
