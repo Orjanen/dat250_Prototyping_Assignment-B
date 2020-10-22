f756df9b1a594cce85ff105849a59b2bacafad90
Kristoffer <31278784+krnome@users.noreply.github.com>
2020-10-22 22:06:24 +0200

    Removed UserEntity.id - caused conflict with Voter.id when logging in

--- api/src/main/java/com/restdeveloper/app/ws/io/entity/UserEntity.java
+++ api/src/main/java/com/restdeveloper/app/ws/io/entity/UserEntity.java
@@ -8,9 +8,7 @@ import java.util.List;
 public class UserEntity extends Voter implements Serializable {
     private static final long serialVersionUID= -3609876517654923756L;
 
-    @Id
-    @GeneratedValue
-    private long id;
+
 
     @Column(nullable = false)
     private String userId;
