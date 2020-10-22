a8e984f9a65d03eb7384a799003d273cbc870891
RogerWisnes <rogerwisnes@gmail.com>
2020-10-22 22:13:58 +0200

    Removed / commented out unused code

--- analytics/src/main/java/com/example/analytics/AnalyticsApplication.java
+++ analytics/src/main/java/com/example/analytics/AnalyticsApplication.java
@@ -1,46 +1,77 @@
 package com.example.analytics;
 
-import com.example.analytics.mongodb.io.entity.PollEntity;
-import com.example.analytics.mongodb.io.repository.PollRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnalyticsApplication {//implements CommandLineRunner {
 
-	@Autowired
-	private PollRepository repository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnalyticsApplication.class, args);
-	}
-/*
-	@Override
-	public void run(String... args) throws Exception {
+    /*
+        @Autowired
+        private PollRepository repository;
 
-		repository.deleteAll();
+        @Override
+        public void run(String... args) throws Exception {
 
-		// save a couple of polls
-		repository.save(new PollEntity("p1", "TestPoll", "Yes", "No", 0, 0));
-		repository.save(new PollEntity("p2", "TestPoll2", "This", "That", 0, 0));
+            repository.deleteAll();
 
-		// fetch all polls
-		System.out.println("Polls found with findAll():");
-		System.out.println("-------------------------------");
-		for (PollEntity poll : repository.findAll()) {
-			System.out.println(poll);
-		}
-		System.out.println();
+            // save a couple of polls
+            repository.save(new PollEntity("p1", "TestPoll", "Yes", "No", 0, 0));
+            repository.save(new PollEntity("p2", "TestPoll2", "This", "That", 0, 0));
 
-		// fetch an individual customer
-		System.out.println("Poll found with findByPollName('TestPoll'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByPollName("TestPoll"));
+            // fetch all polls
+            System.out.println("Polls found with findAll():");
+            System.out.println("-------------------------------");
+            for (PollEntity poll : repository.findAll()) {
+                System.out.println(poll);
+            }
+            System.out.println();
+
+            // fetch an individual customer
+            System.out.println("Poll found with findByPollName('TestPoll'):");
+            System.out.println("--------------------------------");
+            System.out.println(repository.findByPollName("TestPoll"));
+        }
 
 
-	}
+        public static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+        public static final String QUEUE_NAME = "spring-boot";
 
- */
+        @Bean
+        Queue queue() {
+            return new Queue(QUEUE_NAME, false);
+        }
+
+        @Bean
+        TopicExchange exchange() {
+            return new TopicExchange(TOPIC_EXCHANGE_NAME);
+        }
+
+        @Bean
+        Binding binding(Queue queue, TopicExchange exchange) {
+            return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+        }
+
+        @Bean
+        SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                                 MessageListenerAdapter listenerAdapter) {
+            SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+            container.setConnectionFactory(connectionFactory);
+            container.setQueueNames(QUEUE_NAME);
+            container.setMessageListener(listenerAdapter);
+            return container;
+        }
+
+        @Bean
+        MessageListenerAdapter listenerAdapter(Receiver receiver) {
+            return new MessageListenerAdapter(receiver, "receiveMessage");
+        }
+    */
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsApplication.class, args);
+    }
+
 }
+
