bc3f21136eb713db68330cd833f1547426815dcd
RogerWisnes <rogerwisnes@gmail.com>
2020-10-22 23:30:13 +0200

    Added dependency for gson, and created Converter

---
+++ analytics/src/main/java/com/example/analytics/mongodb/handler/util/Converter.java
@@ -0,0 +1,31 @@
+package com.example.analytics.mongodb.handler.util;
+
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+public final class Converter {
+
+    private static final Gson gson = new GsonBuilder()
+            .serializeNulls()
+            .excludeFieldsWithoutExposeAnnotation()
+            .setPrettyPrinting()
+            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+            .create();
+
+    private Converter() {
+    }
+
+    public static String convertToJson(Object object) {
+        return gson.toJson(object);
+    }
+
+    public static JsonObject convertToJsonObject(Object object) {
+        String jsonFormat = convertToJson(object);
+        return gson.fromJson(jsonFormat,JsonObject.class);
+    }
+
+
+}
