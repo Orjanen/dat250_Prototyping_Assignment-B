77fbad90dcfc6078f94e17fb3a84a972e7ba53cd
RogerWisnes <rogerwisnes@gmail.com>
2020-10-23 11:41:58 +0200

    Removed unused code

--- analytics/src/main/java/com/example/analytics/AnalyticsApplication.java
+++ analytics/src/main/java/com/example/analytics/AnalyticsApplication.java
@@ -1,15 +1,10 @@
 package com.example.analytics;
 
-import com.example.analytics.mongodb.io.repository.PollRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnalyticsApplication {
-
-     @Autowired
-     private PollRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsApplication.class, args);
