d088daa56b3ce9c5b7b5173b6ca66acb93876dff
RogerWisnes <rogerwisnes@gmail.com>
2020-10-25 18:17:35 +0100

    Added JsonHandler as a Bean

--- analytics/src/main/java/com/example/analytics/AnalyticsApplication.java
+++ analytics/src/main/java/com/example/analytics/AnalyticsApplication.java
@@ -1,10 +1,18 @@
 package com.example.analytics;
 
+import com.example.analytics.handler.JsonHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnalyticsApplication {
+
+    @Bean
+    public JsonHandler jsonHandler() {
+        return new JsonHandler();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsApplication.class, args);
