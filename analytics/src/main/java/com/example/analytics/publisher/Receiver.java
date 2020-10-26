021c9fd35ada3aa29b17d3dc2306de32c2b7eaca
RogerWisnes <rogerwisnes@gmail.com>
2020-10-26 14:49:52 +0100

    Added 'input-validation' using Converter, and implemented Logger

--- analytics/src/main/java/com/example/analytics/publisher/Receiver.java
+++ analytics/src/main/java/com/example/analytics/publisher/Receiver.java
@@ -1,6 +1,9 @@
 package com.example.analytics.publisher;
 
 import com.example.analytics.handler.JsonHandler;
+import com.example.analytics.handler.util.Converter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,12 +14,14 @@ public class Receiver {
     @Autowired
     JsonHandler jsonHandler;
 
-    @RabbitListener(queues = "#{autoDeleteQueue.name}")
-    public void receive(String message) throws InterruptedException {
-        System.out.println("Analytics-Receiver got message:");
-        System.out.println(message);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
-        jsonHandler.initializeHandlingProcess(message);
+    @RabbitListener(queues = "#{autoDeleteQueue.name}")
+    public void receive(String message) {
+        String messageInSmallPrint = message.replace("\n", "");
+        LOGGER.info("Analytics-Receiver got message: {}", messageInSmallPrint);
+
+        if (Converter.isValidJson(message)) jsonHandler.initializeHandlingProcess(message);
     }
 
 }
