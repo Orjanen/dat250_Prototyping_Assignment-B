cd95feabcbe02c0a68a984b48ee485c06b92858c
RogerWisnes <rogerwisnes@gmail.com>
2020-10-27 12:28:41 +0100

    Implemented Logger, and removed unused methods

--- api/src/main/java/com/restdeveloper/app/ws/publisher/util/Converter.java
+++ api/src/main/java/com/restdeveloper/app/ws/publisher/util/Converter.java
@@ -1,10 +1,17 @@
 package com.restdeveloper.app.ws.publisher.util;
 
-import com.google.gson.*;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.restdeveloper.app.ws.io.entity.PollEntity;
 import com.restdeveloper.app.ws.io.entity.VoteEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Converter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Converter.class);
 
     private static final Gson gson = new GsonBuilder()
             .serializeNulls()
@@ -16,33 +23,30 @@ public final class Converter {
     private Converter() {
     }
 
-    public static String convertToJson(Object object) {
-        return gson.toJson(object);
-    }
-
-    public static JsonObject convertToJsonObject(Object object) {
-        String jsonFormat = convertToJson(object);
-        return gson.fromJson(jsonFormat,JsonObject.class);
-    }
-
     public static String convertPollToJson(PollEntity poll) {
-        System.out.println("Converting PollEntity to Json-string...");
+        LOGGER.info("Converting PollEntity to Json-string...");
+
         JsonObject json = new JsonObject();
         json.addProperty("type", "poll");
         json.addProperty("jpaId", poll.getPollId());
         json.addProperty("pollName", poll.getPollName());
         json.addProperty("optionOne", poll.getOptionOne());
         json.addProperty("optionTwo", poll.getOptionTwo());
+
+        LOGGER.debug("Done converting");
         return gson.toJson(json);
     }
 
     public static String convertVoteToJson(VoteEntity vote) {
-        System.out.println("Converting VoteEntity to Json-string...");
+        LOGGER.info("Converting VoteEntity to Json-string...");
+
         JsonObject json = new JsonObject();
         json.addProperty("type", "vote");
         json.addProperty("jpaId", vote.getPollEntity().getPollId());
         json.addProperty("optionOneVotes", vote.getOption1Count());
         json.addProperty("optionTwoVotes", vote.getOption2Count());
+
+        LOGGER.debug("Done converting");
         return gson.toJson(json);
     }
 
